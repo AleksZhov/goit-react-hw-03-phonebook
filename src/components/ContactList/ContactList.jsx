32a3c4ff0472b1c8cecf2313be11e1dfc4367fa6
AleksZhov <alekszhov@gmail.com>
2022-08-09 23:12:47 +0300

    initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './ContactList.css';
+function ContactList({ contactsArray, onDelete }) {
+  return (
+    <ul className="contacts">
+      {contactsArray.map(({ name, number, id }) => {
+        return (
+          <li key={number} className="contacts__item">
+            <p>
+              • {name} : {number}
+            </p>
+            <button
+              type="button"
+              className="contacts__del-btn"
+              onClick={() => onDelete(id)}
+            >
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+ContactList.propTypes = {
+  contactsArray: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+      number: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    })
+  ),
+};
+export default ContactList;
